5bb6b140004b96663118aab5a4774cf3c7deeed0
ssmith2006 <shantel.smith@hotmail.com>
2025-08-01 03:02:26 +0000

    completed assigned function using map, filter, and arrow functions

---
+++ passingStudents.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const students = [
+  { name: "Ana", score: 90 },
+  { name: "Luis", score: 65 },
+  { name: "Emma", score: 85 },
+];
+
+function getPassingStudentNames(students){
+   const passing= students.filter(student => student.score >= 70);
+   const names = passing.map(student => student.name);
+   return names;  
+}
+console.log(getPassingStudentNames(students));
+
